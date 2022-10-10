5c580db8e24ff79717cccccb8f41bed34e4f12a0
pranjalipawashe <111579668+pranjalipawashe@users.noreply.github.com>
2022-10-10 13:36:10 +0530

    updated code

--- PayMoney/src/com/greatlearning/paymoney/driver/Main.java
+++ PayMoney/src/com/greatlearning/paymoney/driver/Main.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 public class Main {
 	public static void checkTarget(int target, int[] arr, int size)
 	{
-		int checkVar = 0;
+	    int checkVar = 0;
 	    int idx = 1;
 	    boolean chkCondtn = false;
 	
 	    for( int i =0; i < size ; i++)
 	    { 
-		    checkVar += arr[i];
+		checkVar += arr[i];
 		
     		if(checkVar >= target)
 	    	{
-		    	chkCondtn = true;
-			    System.out.println("Target achieved after " + idx + " transactions" );
-			    break;
-		    }
+		    chkCondtn = true;
+		    System.out.println("Target achieved after " + idx + " transactions" );
+		    break;
+	        }
 	    	idx+= 1;
 	    }
-    	if(chkCondtn == false)
+    	    if(chkCondtn == false)
 	    {
 	        System.out.println("Given target is not achieved");
 	    }
-    }
+        }
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
@@ -35,7 +35,7 @@ public class Main {
 		int[] arr = new int[size];
 		for(int i=0; i<size; i++)
 		{
-			arr[i] = sc.nextInt();
+	            arr[i] = sc.nextInt();
 		}
 		
 		System.out.println("Enter the total number of targets to be achieved ");
@@ -45,9 +45,9 @@ public class Main {
 		int[] target = new int[targets];
 		for(int i=0; i<targets; i++)
 		{
-			System.out.println("\nEnter the value of target " );
-			target[i] = sc.nextInt();
-			checkTarget(target[i],arr ,size);
+	            System.out.println("\nEnter the value of target " );
+		    target[i] = sc.nextInt();
+		    checkTarget(target[i],arr ,size);
 		}
 				
 	}
