48b063a801f421d346429f0a5d1225b4863e0411
pranjalipawashe <111579668+pranjalipawashe@users.noreply.github.com>
2022-10-10 13:34:33 +0530

    updated code

--- CurrencyDenomination/src/com/greatlearning/currencydenomination/driver/MergeSort.java
+++ CurrencyDenomination/src/com/greatlearning/currencydenomination/driver/MergeSort.java
@@ -2,22 +2,21 @@ package com.greatlearning.currencydenomination.driver;
 //this code will sort the data in descending order
 public class MergeSort {
 	static int[] merge(int[] arr, int left, int mid, int right ) {
-		int len1 = mid-left+1;
-		int len2 = right - mid;
-		
-		int[] leftArr = new int[len1];
-        int[] rightArr = new int[len2];
+	    int len1 = mid-left+1;
+	    int len2 = right - mid;
+	    int[] leftArr = new int[len1];
+            int[] rightArr = new int[len2];
         
-        for(int i=0;i<len1;i++) {
+            for(int i=0;i<len1;i++) {
         	leftArr[i] = arr[left+i];
-        }
-        for(int j =0; j<len2; j++) {
+            }
+            for(int j =0; j<len2; j++) {
         	rightArr[j] = arr[mid+1+j];
-        }
+            }
         
-        int i=0, j=0, k=left;
+            int i=0, j=0, k=left;
         
-        while(i<len1 && j<len2) {
+            while(i<len1 && j<len2) {
         	if(leftArr[i] >= rightArr[j])
         	{
         		arr[k]= leftArr[i];
@@ -29,20 +28,20 @@ public class MergeSort {
         		j++;
         	}
         	k++;
-        }
+            }
        
-        while(j<len2) {
+            while(j<len2) {
         	arr[k]=rightArr[j];
         	j++;
         	k++;
-        }
-        while(i<len1) {
+            }
+            while(i<len1) {
         	arr[k]=leftArr[i];
         	i++;
         	k++;
-        }
+            }
 
-        return arr;
+            return arr;
 	}
 	
 	static void mergeSort(int[] arr, int left, int right) {
